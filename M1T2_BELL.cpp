18b6a50b9ba4593967cade54bc680667692c1cec
bellS0158 <144379766+bellS0158@users.noreply.github.com>
2024-01-22 16:17:01 +0000

    Finished Assignment

---
+++ M1T2_BELL.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M1T2
+// Schefeyon Bell
+// 1/22/24
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Introduce Youself 
+    cout << "Hello World!" << endl;
+    cout << "My name is Schefeyon Bell." << endl;
+    cout << "My Major is IT." << endl;
+    cout << "I love to work with computers and with my hands." << endl; 
+
+    // Ask a Question
+    string name; 
+    cout << "Whats your name?" << endl;
+    cin >> name;
+    
+    cout << "Nice to meet you, " << name << endl;
+
+
+    cout<< endl << endl;
+    return 0;
+}
+
