c65076b0c35e56602c0e5dfe2d14650c7a8876c2
bellS0158 <144379766+bellS0158@users.noreply.github.com>
2024-02-19 16:19:30 +0000

    first part done.. could make more changes

---
+++ M3/M3_lab1.cpp
@@ -0,0 +1,60 @@
+// if statement example2
+// standard header
+
+
+// compiler directives
+// (required for the program to run)
+#include <iostream>
+using namespace std;
+
+// beginning of the main() method
+int main() {
+  
+  // this program will ask a question and respond to it.
+  // You should run it, and test it by typing in different values.
+  // Example test values: 1, 2, 3, banana (try all of them)
+
+  // declare the variable we need
+  int choice; 
+
+  // ask the question
+  cout << "Do you like Box Chevys or Bubble Chevys." << endl;
+  cout << "Type 1 for Box or 2 for Bubble: "; 
+  cin >> choice;
+
+  // using if, make a decision based on the user's choice
+
+  if (1 == choice) {
+  	cout << "You chose BOX CHEVYS BABYYYYY" << endl;
+    cout << "Would you like 24 or 28 inch rims?" << endl;
+    cout << "Type 1 for 28s or 2 for 24s: ";
+    cin >> choice;
+        if (1 == choice) {
+            cout << "Big boy 28s" << endl;
+        }
+        else if (2 == choice) {
+            cout << "Nice size.. BUT GO BIG BABY" << endl;
+        }
+
+  }
+  else if (2 == choice) {
+  	cout << "You chose BUBBLES IN THE HOUSE" << endl;
+    cout << "Would you like 24 or 28 inch rims?" << endl;
+    cout << "Type 1 for 28s or 2 for 24s: "; 
+    cin >> choice;
+      if (1 == choice) {
+        cout << " Big boy 28s" << endl; 
+      }  
+      else if (2 == choice) {
+        cout << "Nice size.. BUT GO BIG BABY" << endl; 
+      }
+  }
+  else {
+  	cout << "I'm sorry, that is not a valid choice." << endl;
+  }
+
+  // finish up
+  cout << "Thanks for playing!" << endl; // this runs no matter what they choose
+  return 0; // exit without error
+
+} // end of the main() method
---
+++ M3/Starter.cpp
@@ -0,0 +1,8 @@
+// Use as starter for any program 
+// CSC 134
+// Assignment 
+// Schefeyon Bell 
+// 02/19/24
+
+#include <iostream>
+using namespace std; 
